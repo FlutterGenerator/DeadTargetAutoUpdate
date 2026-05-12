1059861334127e4bfd3286a95665c2a074b209d5
tojik_proof_93 <elementman50@yandex.com>
2026-05-12 23:40:34 +0500

    Update Main.java

--- app/src/main/java/com/android/support/Main.java
+++ app/src/main/java/com/android/support/Main.java
@@ -11,29 +11,30 @@ import android.widget.Toast;
 
 public class Main {
 
-    //Load lib
-    static {
-        // When you change the lib name, change also on Android.mk file
-        // Both must have same name
-        System.loadLibrary("MyLibName");
-    }
-
-    private static native void CheckOverlayPermission(Context context);
-
-    public static void StartWithoutPermission(Context context) {
-
-        if (context instanceof Activity) {
-            //Check if context is an Activity.
-            Menu menu = new Menu(context);
-            menu.SetWindowManagerActivity();
-            menu.ShowMenu();
-        } else {
-            //Anything else, ask for permission
-            CheckOverlayPermission(context);
+        // Load lib
+        static {
+                // When you change the lib name, change also on Android.mk file
+                // Both must have same name
+                System.loadLibrary("MyLibName");
         }
-    }
 
-    public static void Start(Context context) {
-        CheckOverlayPermission(context);
-    }
+        private static native void CheckOverlayPermission(Context context);
+
+        public static void StartWithoutPermission(Context context) {
+                CrashHandler.init(context, true);
+                if (context instanceof Activity) {
+                        // Check if context is an Activity.
+                        Menu menu = new Menu(context);
+                        menu.SetWindowManagerActivity();
+                        menu.ShowMenu();
+                } else {
+                        Toast.makeText(context, "Failed to launch the mod menu\n", Toast.LENGTH_LONG).show();
+                }
+        }
+
+        public static void Start(Context context) {
+                CrashHandler.init(context, false);
+
+                CheckOverlayPermission(context);
+        }
 }
